85f2f0cc216d3a8d18b3e3b22668f238b044bdaa
CrisNguyen <nguyenvantrieu.qngdp@gmail.com>
2022-12-21 23:48:31 +0700

    create customer model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_10_080257) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_21_164605) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "customers", force: :cascade do |t|
+    t.string "full_name"
+    t.string "email"
+    t.string "contact_number"
+    t.string "stripe_customer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "workshops", force: :cascade do |t|
     t.string "name"
