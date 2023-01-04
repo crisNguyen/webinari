49328add463604b7991eedd343eb69255b66c49f
CrisNguyen <nguyenvantrieu.qngdp@gmail.com>
2023-01-04 23:17:10 +0700

    add gem stripe

---
+++ app/models/booking.rb
@@ -0,0 +1,12 @@
+class Booking < ApplicationRecord
+  belongs_to :customer
+  belongs_to :workshop
+
+  after_create :update_workshop_sit_count
+
+  private
+
+  def update_workshop_sit_count
+    workshop.update(remaining_sits: workshop.total_sits - no_if_ticket)
+  end
+end
